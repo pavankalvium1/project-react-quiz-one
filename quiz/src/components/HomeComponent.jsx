64f301b726b269d21008146e8edabeae2f594574
pavankalvium1 <pavanew11@gmail.com>
2024-02-10 10:46:32 +0530

    updated

---
+++ quiz/src/components/HomeComponent.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import "../App.css"
+
+class HomeComponent extends Component {
+  render() {
+    return (
+        <body class="home">
+            
+        
+      <div class="homecomp">
+
+        <h1 id='homeH1'>Quiz App</h1>
+        <button id="play" >Play</button>
+
+      </div>
+      </body>
+    )
+  }
+}
+
+export default HomeComponent
